4566657c6bae3130a69bf62f3ecd001d1475128a
lucascpd@unb.br <lucascpd@unb.br>
2019-03-22 16:03:46 -0300

    corrigindo erro

--- generators/app/templates/complementos.java
+++ generators/app/templates/complementos.java
@@ -2,12 +2,12 @@
 private <%-nomeTitleCase%>Repository <%-nomeVariableCase%>Repository;
 
 public <%-nomeTitleCase%>Repository get<%-nomeTitleCase%>Repository() {
-    return situacaoPlanoRepository;
+    return <%-nomeVariableCase%>Repository;
 }
 
 
 @EJB private <%-nomeTitleCase%>Service <%-nomeVariableCase%>Service;
 
 public <%-nomeTitleCase%>Service get<%-nomeTitleCase%>Service() {
-    return <%-nomeTitleCase%>Service;
+    return <%-nomeVariableCase%>Service;
 }
